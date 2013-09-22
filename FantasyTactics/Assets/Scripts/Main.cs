aadd73d4ac0cdb6f4b683467a2c2689ace1b07fd
CharlesVI <charles.hotaling@gmail.com>
2013-09-22 12:17:48 -0400

    Click logic and ID complete, moving on to movement logic

--- FantasyTactics/Assets/Scripts/Main.cs
+++ FantasyTactics/Assets/Scripts/Main.cs
@@ -11,7 +11,7 @@ public class Main : MonoBehaviour {
     Tile[,] tiles = new Tile[8, 8];
 
     public GameObject pieceGO;
-    Piece[] piece = new Piece[16];
+    Piece[] pieces = new Piece[32];
    
 
     Ray ray;
@@ -55,9 +55,108 @@ public class Main : MonoBehaviour {
 
     void SetupPieces()
     {
-        piece[1] = new Piece();
-        piece[1].gameObject = (GameObject)Instantiate(pieceGO, new Vector3(3, 1, 3), Quaternion.identity);
+        for (int ii = 0; ii < 32; ii++)
+        { 
+            pieces[ii] = new Piece();
+            pieces[ii].gameObject = (GameObject)Instantiate(pieceGO, new Vector3(0, 0, 0), Quaternion.identity);
+
+            if(ii < 16)
+            {
+                pieces[ii].player = 1;
+                pieces[ii].gameObject.transform.renderer.material.color = Color.red;
+            }
+
+            if(ii > 15)
+            {
+                pieces[ii].player = 2;
+                pieces[ii].gameObject.transform.renderer.material.color = Color.blue;
+            }
+        }
+        
+        //At this point I'm just going to maunally set the pieces up. Either it will always be this set up or some level of custom work will be involved and i'll loop through a list per player.
+        
+        //Player 1 This really should be a list. However there have been issues with lists interacting
+        //refrencing all one thing and i'm comfortable with the array.
+        int nn = 0;
+        pieces[nn].SetPositionOneAbove(tiles[7, 7].gameObject);
+        pieces[nn].type = Piece.Type.ROOK;
+        nn++;
+
+        pieces[nn].SetPositionOneAbove(tiles[6, 7].gameObject);
+        pieces[nn].type = Piece.Type.KNIGHT;
+        nn++;
+
+        pieces[nn].SetPositionOneAbove(tiles[5, 7].gameObject);
+        pieces[nn].type = Piece.Type.BISHOP;
+        nn++;
+
+        pieces[nn].SetPositionOneAbove(tiles[4, 7].gameObject);
+        pieces[nn].type = Piece.Type.KING;
+        nn++;
+
+        pieces[nn].SetPositionOneAbove(tiles[3, 7].gameObject);
+        pieces[nn].type = Piece.Type.QUEEN;
+        nn++;
+
+        pieces[nn].SetPositionOneAbove(tiles[2, 7].gameObject);
+        pieces[nn].type = Piece.Type.BISHOP;
+        nn++;
+
+        pieces[nn].SetPositionOneAbove(tiles[1, 7].gameObject);
+        pieces[nn].type = Piece.Type.KNIGHT;
+        nn++;
+
+        pieces[nn].SetPositionOneAbove(tiles[0, 7].gameObject);
+        pieces[nn].type = Piece.Type.ROOK;
+        nn++;
+
+        for(int ii = 0; ii < 8; ii ++)
+        {
+            pieces[nn].SetPositionOneAbove(tiles[ii, 6].gameObject);
+            pieces[nn].type = Piece.Type.PAWN;
+            nn++;
+        }
+
+        pieces[nn].SetPositionOneAbove(tiles[7, 0].gameObject);
+        pieces[nn].type = Piece.Type.ROOK;
+        nn++;
+
+        pieces[nn].SetPositionOneAbove(tiles[6, 0].gameObject);
+        pieces[nn].type = Piece.Type.KNIGHT;
+        nn++;
+
+        pieces[nn].SetPositionOneAbove(tiles[5, 0].gameObject);
+        pieces[nn].type = Piece.Type.BISHOP;
+        nn++;
+
+        pieces[nn].SetPositionOneAbove(tiles[4, 0].gameObject);
+        pieces[nn].type = Piece.Type.KING;
+        nn++;
+
+        pieces[nn].SetPositionOneAbove(tiles[3, 0].gameObject);
+        pieces[nn].type = Piece.Type.QUEEN;
+        nn++;
+
+        pieces[nn].SetPositionOneAbove(tiles[2, 0].gameObject);
+        pieces[nn].type = Piece.Type.BISHOP;
+        nn++;
+
+        pieces[nn].SetPositionOneAbove(tiles[1, 0].gameObject);
+        pieces[nn].type = Piece.Type.KNIGHT;
+        nn++;
+
+        pieces[nn].SetPositionOneAbove(tiles[0, 0].gameObject);
+        pieces[nn].type = Piece.Type.ROOK;
+        nn++;
+
+        for (int ii = 0; ii < 8; ii++)
+        {
+            pieces[nn].SetPositionOneAbove(tiles[ii, 1].gameObject);
+            pieces[nn].type = Piece.Type.PAWN;
+            nn++;
+        }
     }
+       
 
     // Update is called once per frame
     void Update()
@@ -67,9 +166,28 @@ public class Main : MonoBehaviour {
         {
             ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            if (Physics.Raycast(ray, out hit))
+            if (Physics.Raycast(ray, out hit, 200.0f))
             {
-                //DO click shit.
+                string tagPiece = "Piece";
+                string tagTile = "Tile";
+
+                if (hit.collider.gameObject.tag == tagPiece)
+                {
+                    Debug.Log("I hit a piece");
+                    Debug.Log("it is at " + hit.collider.gameObject.transform.position + "and is a " + hit.collider.gameObject.tag);
+                    foreach(Piece piece in pieces)
+                    {
+                        if (hit.collider.gameObject.transform.position == piece.gameObject.transform.position)
+                        {
+                            Debug.Log("I am a " + piece.type);
+                        }
+                    }
+                }
+
+                if (hit.collider.gameObject.tag == tagTile)
+                {
+                    Debug.Log("shot a tile");
+                }
             }
             
         }
--- FantasyTactics/Assets/Scripts/Piece.cs
+++ FantasyTactics/Assets/Scripts/Piece.cs
@@ -5,11 +5,16 @@ public class Piece
 {
     public GameObject gameObject;
     public enum Type {PAWN, ROOK, KNIGHT, BISHOP, QUEEN, KING };
+    public Type type;
     public int player;
     public Color color;
     public bool canMove;
     public bool active;
 
-
+    public void SetPositionOneAbove(GameObject go)
+    {
+        this.gameObject.transform.position = new Vector3(go.transform.position.x,
+            go.transform.position.y + 1, go.transform.position.z);
+    }
 
 }
